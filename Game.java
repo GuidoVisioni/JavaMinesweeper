b391852f81e1030336e73730377b82fec7d602ff
Guido Visioni <gjvisioni@gmail.com>
2020-06-27 20:42:51 -0700

    Game

--- Game.java
+++ Game.java
@@ -1,4 +1,19 @@
 public class Game 
 {
+    public static void main(String[] args)
+    {
+        showInstructions();
+    }
 
+    String currentRoom = "Hall";
+
+    static void showInstructions()
+    {
+       System.out.println("Commands: go [direction], get [item]");
+    }
+
+    static void showStatus()
+    {
+    
+    }
 }
